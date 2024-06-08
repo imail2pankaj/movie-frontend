881cf7ea804e976672a8a0918c74aa9267694cba
Pankaj Makwana <imail2pankaj@gmail.com>
2024-06-09 02:08:29 +0530

    FIlter Implementing

--- app/peoples/page.jsx
+++ app/peoples/page.jsx
@@ -21,7 +21,7 @@ export async function generateMetadata({ params }, parent) {
 
 const Peoples = async () => {
 
-  // const persons = await fetchFilteredPersons({});
+  const persons = await fetchFilteredPersons({});
 
   return (
     <>
