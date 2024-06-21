33a55a16fde49b8959584077672d1e2146c7aed1
Pankaj Makwana <imail2pankaj@gmail.com>
2024-06-21 18:53:17 +0530

    Google AdSense

--- app/_components/GoogleAdsense.jsx
+++ app/_components/GoogleAdsense.jsx
@@ -7,12 +7,12 @@ const GoogleAdsense = () => {
   return (
     <>
       <ins class="adsbygoogle"
-        style={{ display: "block" }}
+        style={{ display: "block", width: 728, height: 90 }}
         data-ad-client="ca-pub-6282693958918202"
         data-ad-slot="6944255980"
         data-ad-format="auto"
         data-full-width-responsive="true"></ins>
-      <Script id="adcode-id" dangerouslySetInnerHTML={{ __html: "(window.adsbygoogle = window.adsbygoogle || []).push({});" }} />
+      <Script id="ca-pub-6282693958918202" dangerouslySetInnerHTML={{ __html: "(window.adsbygoogle = window.adsbygoogle || []).push({});" }} />
     </>
   );
 };
