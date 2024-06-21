2f2cecb58aa8456976fee055e818316760d07a89
Pankaj Makwana <imail2pankaj@users.noreply.github.com>
2024-06-21 13:01:23 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -32,16 +32,16 @@ export const metadata = {
   },
 };
 
-      // {process.env.NODE_ENV === 'production' && <head>
-      //   {/* <meta name="google-site-verification" content="sKCF8N5RL_s-B01eLKxvcM-rWyjyDEQ8-yPNqQ8dIWU" />
-      //   <meta name="google-adsense-account" content="ca-pub-6282693958918202" /> 
-      //   <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6282693958918202" crossOrigin="anonymous" strategy="lazyOnload" />*/}
-      // </head>}
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-
+      {process.env.NODE_ENV === 'production' && <head>
+           <meta name="google-site-verification" content="sKCF8N5RL_s-B01eLKxvcM-rWyjyDEQ8-yPNqQ8dIWU" />
+         <meta name="google-adsense-account" content="ca-pub-6282693958918202" /> 
+         <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6282693958918202" crossOrigin="anonymous" strategy="lazyOnload" />
+      </head>}
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
@@ -55,7 +55,7 @@ export default function RootLayout({ children }) {
           </main>
           <Footer />
         </div>
-        {/* <GoogleAdsense /> */}
+        <GoogleAdsense />
       </body>
     </html>
   );
