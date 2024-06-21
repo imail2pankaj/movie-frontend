c49406630381d429e40b1954e4a63e58b139d73f
Pankaj Makwana <imail2pankaj@gmail.com>
2024-06-21 19:11:56 +0530

    Adsense setup

--- app/_components/GoogleAdsense.jsx
+++ app/_components/GoogleAdsense.jsx
@@ -1,6 +1,12 @@
+"use client"
 import Script from "next/script";
+import { useEffect } from "react";
 
 const GoogleAdsense = () => {
+
+  useEffect(() => {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }, [])
   if (process.env.NODE_ENV !== "production") {
     return null;
   }
@@ -12,7 +18,7 @@ const GoogleAdsense = () => {
         data-ad-slot="6944255980"
         data-ad-format="auto"
         data-full-width-responsive="true"></ins>
-      <Script id="ca-pub-6282693958918202" dangerouslySetInnerHTML={{ __html: "(window.adsbygoogle = window.adsbygoogle || []).push({});" }} />
+      {/* <Script id="ca-pub-6282693958918202" dangerouslySetInnerHTML={{ __html: "" }} /> */}
     </>
   );
 };
